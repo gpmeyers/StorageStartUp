9ede95d95b353cf595e413f1d9b152bf84f17032
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-05-22 01:33:41 -0700

    Added Data file for info sections and completed first section, image does not work though.

---
+++ react-storage-startup/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Effortless Storage',
+    headline: 'Premium storage service all from home',
+    description: 'Get access to one of our packages that allow you to access new storage space all from your computer at home.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: '',
+    dark: true,
+    primary: true,
+    darkText: false
+};
