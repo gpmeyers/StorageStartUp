bdfe6a44f1c6e733dbd8ba023cb785d94a5b63be
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-05-22 18:29:15 -0700

    Added more info sections and the services section, infor needs to be updated for services section.

--- react-storage-startup/src/components/InfoSection/Data.js
+++ react-storage-startup/src/components/InfoSection/Data.js
@@ -9,8 +9,42 @@ export const homeObjOne = {
     buttonLabel: 'Get Started',
     imgStart: false,
     img: require('../../images/svg-1.svg'),
-    alt: '',
+    alt: 'Woman with packages',
     dark: true,
     primary: true,
     darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Fast Access to Service',
+    headline: 'Login to find out more about what we offer',
+    description: 'Follow our simple steps on how to have your items picked up or delivered. You can start now, or learn more about the process',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Woman selecting options for purchase',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join Today and Enjoy Your New Space',
+    headline: 'Creating an account is fast and easy',
+    description: 'Sign up now to immediately gain access to your new repository of space and start shipping instantly.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Man signing up',
+    dark: false,
+    primary: false,
+    darkText: true
 };
