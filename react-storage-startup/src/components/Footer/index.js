75f297297c9c8b687183fa57f44cc82b7b8023b4
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-05-22 19:37:07 -0700

    Added Footer to page.

---
+++ react-storage-startup/src/components/Footer/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/'>How it works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to='/'>Contact</FooterLink>
+                            <FooterLink to='/'>Support</FooterLink>
+                            <FooterLink to='/'>Destinations</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            New Wave Storage
+                        </SocialLogo>
+                        <WebsiteRights>New Wave Storage {new Date().getFullYear()}</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    );
+}
+
+export default Footer;
