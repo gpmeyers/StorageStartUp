2067263ec3d4418f2c8e69070788cf4d3fd2e8b3
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-05-23 01:38:03 -0700

    Made changes to the services section of the page.

--- react-storage-startup/src/components/Services/index.js
+++ react-storage-startup/src/components/Services/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Icon1 from '../../images/svg-1.svg';
-import Icon2 from '../../images/svg-2.svg';
-import Icon3 from '../../images/svg-3.svg';
+import Icon1 from '../../images/svg-4.svg';
+import Icon2 from '../../images/svg-5.svg';
+import Icon3 from '../../images/svg-6.svg';
 import {
     ServicesContainer,
     ServicesH1,
@@ -19,18 +19,18 @@ const Services = () => {
             <ServicesWrapper>
                 <ServicesCard>
                     <ServicesIcon src={Icon1} />
-                    <ServicesH2>Reduces Expenses</ServicesH2>
-                    <ServicesP>We help reduce your fees and increase your overall revenue.</ServicesP>
+                    <ServicesH2>Organized Containers</ServicesH2>
+                    <ServicesP>We will provide you with builk containers and smaller totes to keep your items organized and safe.</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon2} />
-                    <ServicesH2>Virtual Offices</ServicesH2>
-                    <ServicesP>You can access our platform online anywhere in the world.</ServicesP>
+                    <ServicesH2>Secure Tracking</ServicesH2>
+                    <ServicesP>Each container and tote will have a unique code allowing the user to know the status of their items at all times.</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon3} />
-                    <ServicesH2>Premium Benefits</ServicesH2>
-                    <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
+                    <ServicesH2>Usable Interface</ServicesH2>
+                    <ServicesP>Our web and application interfaces allow you to easily send, receive, and track your items with ease.</ServicesP>
                 </ServicesCard>
             </ServicesWrapper>
         </ServicesContainer>
